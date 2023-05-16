ca280e177e61f8fb3d38370fb4fcbd875a1a6a84
jcalixto202020774 <j.calixto@uniandes.edu.co>
2023-05-16 09:25:17 -0500

    Actividad

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { FormattedDate, FormattedMessage } from 'react-intl';
+import { FormattedNumber, FormattedPlural } from 'react-intl';
 
 const Job = (props) => {
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td><FormattedMessage id={props.offer.name}/></td>
+      <td><FormattedMessage id={props.offer.company}/></td>
+      <td>
+        {props.offer.salary.toLocaleString()}
+        {" "}
+        <FormattedPlural
+          value={props.offer.salary}
+          one={<FormattedMessage id="million" />}
+          other={<FormattedMessage id="millions" />}
+        />
+      </td>
+      <td><FormattedMessage id={props.offer.city}/></td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td><FormattedMessage id={props.offer.views.toLocaleString()}/></td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const navLang = navigator.language;
+let messages;
+if (navLang === "es") {
+    messages = localeEsMessages; // Utiliza los mensajes en español
+} else if (navLang === "en") {
+    messages = localeEnMessages; // Utiliza los mensajes en inglés como valor predeterminado
+}
+
+ReactDOM.render(
+    <IntlProvider locale={navLang} messages={messages}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
